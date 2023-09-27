2e3a2c6f1ff1978d5c462452ff9f50505cd83db3
yasirqureshi <sir.qurashi@gmail.com>
2023-09-27 16:00:52 +0500

    chapter 1 to chapter 6

---
+++ app.js
@@ -0,0 +1,155 @@
+
+// class 3 java script
+
+// Chapter 5
+var a = 5
+var b = 7
+
+console.log(5*2+
+    ("2"-3)/6)
+
+console.log(5*2+
+("2"+3)/6)
+
+console.log(5*"2"+
+(2+3)/6)
+
+console.log("5"*2+
+(2+3)/6)
+
+console.log(5*2+(2+3)/6)
+
+             5-7 * 12 / 35
+console.log(a-b* (a + b)/(a*b));
+               12 *   35
+console.log((a + b)*(a*b));
+            5+7+5*7
+console.log(a + b +(a*b));
+             5 + 7
+console.log(a + b);
+var a = 2;
+var b = 3;
+console.log(a+b);
+var a = 2;
+var b = 3;
+var c = 6;
+console.log(a+b*c);
+var a = 2;
+var b = 3;
+var c = 6;
+console.log((a+b)*c);
+var a = "2";
+var b = 3;
+var c = 6;
+console.log((a+b)*c);
+var a = "2";
+var b = 3;
+var c = 6;
+console.log((a-b)*c);
+var a = "2";
+var b = 3;
+var c = 6;
+console.log((a-b)*c/a);
+
+// reminder
+// 3*4 ans 0
+var a = 12;
+var b = 3;
+
+console.log(a%b);
+// 3*4 ans 1balance
+var a = 13;
+var b = 3;
+
+console.log(a%b);
+// chapter 6 math expression familiar & unfamiliar
+// increment & decrement 
+// q++ POST - INCREMENT PEHLAI APKI VALUE PRINT HOGI PHIR UPDATE HOGI
+// q-- POST - DECREMENT PEHLAI APKI VALUE PRINT HOGI PHIR UPDATE HOGI
+// ++q PRE  - INCREMENT PEHLAI VALUE UPDATE HOGI PHIR PRINT HOGI
+// --q PRE  - DECREMENT PEHLAI VALUE UPDATE HOGI PHIR PRINT HOGI
+
+var q = 2;
+console.log(++q);
+var q = 2;
+console.log(--q);
+
+var q = 2;
+console.log(q++);
+console.log(q);
+// example
+var a = 2;
+var b = 3;
+var c = ++a + 3
+console.log(c);
+
+var a = 2;
+var b = 3;
+var c = --a + 3
+console.log(c);
+
+var a = 2;
+var b = 3;
+var c = a++ + 3
+console.log(a);
+// console.log(c);
+
+var a = 2;
+var b = 3;
+         2  + 4   
+var c = a++ + ++a + ++b + b++
+console.log(a);
+console.log(b);
+
+var a = 2;
+var b = 3;
+         2  + 4   
+var c = a++ - --a + ++a + --a
+console.log(a);
+console.log(c);
+
+var a = 2;
+var b = 3;
+    
+var c = ++a + ++a - ++a + a-- + b
+console.log(a);
+console.log(c);
+
+var a = 4;
+var b = 6;
+    
+var c = ++b - b + ++a - ++a +b
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+// class 1 java script
+alert("work 1.1 :-"+"  "+"Hello Visitors have your nice day!");
+alert ( "Thanks for your vist!" );
+alert("work 1.2 :-"+"  "+"Error!Please enter a valid password");
+alert("work 1.3 :-"+"  "+"wellcome to js Land....\n Happy Coding!");
+alert("work 1.4 :-"+"  "+"wellcome to js Land....");
+alert("work 1.4.1 :-"+"  "+"Happy Coding!\n Prevent this page frome creating additional dialogs.")
+alert("work 1.5 :-"+"  "+"Hello... I can runJS through my web browse's console")
+// class 2 java script
+var myName = "work 2.1 :-"+"  "+"my name is yasir rasheed"
+var myAge = "work 2.2 :-"+"  "+"39 years old"
+var course = "work 2.3 :-"+"  "+"Certified Mobile Application Development"
+var pizza = "work 2.4 :-"+"  "+"pizza\npizz\npiz\npi\np"
+var email = "work 2.5 :-"+"  "+"my email address is sir.qurashi@gmail.com"
+var education = "work 2.6 :-"+"  "+"I am trying to learn from the book A smarter way to learn JavaScript"
+alert(myName);
+alert(myAge);
+alert(course);
+alert(pizza);
+alert(email);
+alert(education);
+alert("work 2.7 :-"+"  "+"▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+var console = "<h1>Please Check Console</h1>";
+var textBefore = "";
+var textAfter = "<h1>Yasir Rasheed CCO - 118488</h1>";
+
+
+document.write(console + "<br>"+textAfter);
